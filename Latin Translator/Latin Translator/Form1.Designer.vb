622ba2b8f7a5e0f1705a4986c7962b835a33ac5a
jh145216 <31322578+jh145216@users.noreply.github.com>
2017-11-02 11:10:26 -0500

    update Latin Translator

---
+++ Latin Translator/Latin Translator/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSinister = New System.Windows.Forms.Button()
+        Me.btnMedium = New System.Windows.Forms.Button()
+        Me.btnDexter = New System.Windows.Forms.Button()
+        Me.lblLeft = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblCenter = New System.Windows.Forms.Label()
+        Me.lblRight = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSinister
+        '
+        Me.btnSinister.Location = New System.Drawing.Point(33, 128)
+        Me.btnSinister.Name = "btnSinister"
+        Me.btnSinister.Size = New System.Drawing.Size(75, 23)
+        Me.btnSinister.TabIndex = 0
+        Me.btnSinister.Text = "sinister"
+        Me.btnSinister.UseVisualStyleBackColor = True
+        '
+        'btnMedium
+        '
+        Me.btnMedium.Location = New System.Drawing.Point(153, 128)
+        Me.btnMedium.Name = "btnMedium"
+        Me.btnMedium.Size = New System.Drawing.Size(75, 23)
+        Me.btnMedium.TabIndex = 1
+        Me.btnMedium.Text = "medium"
+        Me.btnMedium.UseVisualStyleBackColor = True
+        '
+        'btnDexter
+        '
+        Me.btnDexter.Location = New System.Drawing.Point(281, 128)
+        Me.btnDexter.Name = "btnDexter"
+        Me.btnDexter.Size = New System.Drawing.Size(75, 23)
+        Me.btnDexter.TabIndex = 2
+        Me.btnDexter.Text = "dexter"
+        Me.btnDexter.UseVisualStyleBackColor = True
+        '
+        'lblLeft
+        '
+        Me.lblLeft.AutoSize = True
+        Me.lblLeft.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLeft.Location = New System.Drawing.Point(29, 70)
+        Me.lblLeft.Name = "lblLeft"
+        Me.lblLeft.Size = New System.Drawing.Size(37, 19)
+        Me.lblLeft.TabIndex = 3
+        Me.lblLeft.Text = "Left"
+        Me.lblLeft.Visible = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(153, 181)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblCenter
+        '
+        Me.lblCenter.AutoSize = True
+        Me.lblCenter.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCenter.Location = New System.Drawing.Point(163, 70)
+        Me.lblCenter.Name = "lblCenter"
+        Me.lblCenter.Size = New System.Drawing.Size(55, 19)
+        Me.lblCenter.TabIndex = 5
+        Me.lblCenter.Text = "Center"
+        Me.lblCenter.Visible = False
+        '
+        'lblRight
+        '
+        Me.lblRight.AutoSize = True
+        Me.lblRight.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRight.Location = New System.Drawing.Point(310, 70)
+        Me.lblRight.Name = "lblRight"
+        Me.lblRight.Size = New System.Drawing.Size(46, 19)
+        Me.lblRight.TabIndex = 6
+        Me.lblRight.Text = "Right"
+        Me.lblRight.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 26)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(344, 19)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Click the button to Display the English Translation"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(392, 227)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblRight)
+        Me.Controls.Add(Me.lblCenter)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblLeft)
+        Me.Controls.Add(Me.btnDexter)
+        Me.Controls.Add(Me.btnMedium)
+        Me.Controls.Add(Me.btnSinister)
+        Me.Name = "Form1"
+        Me.Text = "Latin Translator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSinister As System.Windows.Forms.Button
+    Friend WithEvents btnMedium As System.Windows.Forms.Button
+    Friend WithEvents btnDexter As System.Windows.Forms.Button
+    Friend WithEvents lblLeft As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblCenter As System.Windows.Forms.Label
+    Friend WithEvents lblRight As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Latin Translator/Latin Translator/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnSinister_Click(sender As Object, e As EventArgs) Handles btnSinister.Click
+        lblLeft.Visible = True
+    End Sub
+
+    Private Sub btnMedium_Click(sender As Object, e As EventArgs) Handles btnMedium.Click
+        lblCenter.Visible = True
+    End Sub
+
+    Private Sub btnDexter_Click(sender As Object, e As EventArgs) Handles btnDexter.Click
+        lblRight.Visible = True
+    End Sub
+End Class
